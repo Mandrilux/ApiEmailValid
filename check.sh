3bc0acf83f59b7da799f25b6be4e6b6fa6f29c17
heraud_b <baptiste.heraud@epitech.eu>
2018-05-06 18:12:34 +0200

    CHECK

---
+++ check.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+if [ "$#" -eq  "0" ]
+then
+    echo "No arguments supplied"
+    exit 42
+fi
+email=$1
+echo $email
+
+regex="^[a-z0-9!#\$%&'*+/=?^_\`{|}~-]+(\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*@([a-z0-9]([a-z0-9-]*[a-z0-9])?\.)+[a-z0-9]([a-z0-9-]*[a-z0-9])?\$"
+
+if [[ $email =~ $regex ]] ; then
+    echo "FORMAT EMAIL OK"
+else
+    echo "format email invalide"
+    exit 42 
+fi
+
+domain="$(cut -d @ -f2 <<<$email)"
+echo "domaine = " $domain
+dns=0
+nslookup -q=mx $domain | grep "mail exchanger" && dns=1
+if [ $dns == 1 ]
+then
+    echo "DNS OK"
+else
+       echo "DNS KO"
+       exit 42
+fi
+mx="$(nslookup -q=mx $domain | grep "mail exchanger" | head -n 1 | cut -d ' ' -f 5)"
+
+echo "le mx  = " $mx
+#(sleep 1; echo "helo hi"; sleep 1; echo "mail from: <toto@gmail.com>"; sleep 1; echo "rcpt to: <dailybet@dailybet.fr>"; sleep 1; echo "exit") | telnet mail.dailybet.fr 25
+(sleep 1; echo "helo hi"; sleep 1; echo "mail from: <toto@gmail.com>"; sleep 1; echo "rcpt to: <$email>"; sleep 1; echo "exit") | telnet $mx 25
